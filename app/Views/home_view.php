83f1a749a32af80736e08fd52c7430aa194a90dc
Keyan <charleskeyanmarkt@gmail.com>
2025-08-05 23:17:53 +0800

    Added template view and updated Home controller

---
+++ app/Views/home_view.php
@@ -0,0 +1,17 @@
+<div class="text-center py-5">
+    <h1 class="display-4"><?= $title ?></h1>
+    <p class="lead">This is a CodeIgniter project with Bootstrap!</p>
+    
+    <!-- Bootstrap-styled button -->
+    <a href="#" class="btn btn-primary btn-lg mt-3">
+        Click Me
+    </a>
+    
+    <!-- Bootstrap card example -->
+    <div class="card mt-5 mx-auto" style="max-width: 500px;">
+        <div class="card-body">
+            <h5 class="card-title">Lab Exercise 1</h5>
+            <p class="card-text">Successfully integrated Bootstrap!</p>
+        </div>
+    </div>
+</div>
